257072acd9e26d3d63f139124dbae73547c3bcee
Kimjunyoung <985151@naver.com>
2023-11-16 14:38:23 +0900

    first commit

---
+++ src/main/java/com/kleague/kleaguefinder/KleaguefinderApplication.java
@@ -0,0 +1,13 @@
+package com.kleague.kleaguefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KleaguefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KleaguefinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kleague/kleaguefinder/KleaguefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleague.kleaguefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KleaguefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
