74f22691bc5938987994b8c24732542e7b45b0b6
Kimjunyoung <985151@naver.com>
2023-11-16 15:17:09 +0900

    test2 commit

--- src/main/java/com/kleague/kleaguefinder/domain/Member.java
+++ src/main/java/com/kleague/kleaguefinder/domain/Member.java
@@ -1,2 +1,16 @@
-package com.kleague.kleaguefinder.domain;public class Member {
+package com.kleague.kleaguefinder.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Member {
+
+    private Long id;
+    private String memberName;
+    private String memberId;
+    private String memberPassword;
+
+    public Member() {
+    }
+
 }
--- src/main/java/com/kleague/kleaguefinder/domain/Post.java
+++ src/main/java/com/kleague/kleaguefinder/domain/Post.java
@@ -1,2 +1,19 @@
-package com.kleague.kleaguefinder.domain;public class Post {
+package com.kleague.kleaguefinder.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+public class Post {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public Post() {
+    }
+
+
 }
